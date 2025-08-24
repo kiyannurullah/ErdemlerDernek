dfaf221514a021da2fc13a6bae9bda661b63ecda
nkiyan <nurullahkiyan@gmail.com>
2025-08-24 21:44:56 +0300

    vercel file added

--- app.js
+++ app.js
@@ -236,4 +236,6 @@ app.post('/cikis', (req, res) => {
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Sunucu ${PORT} portunda çalışıyor`);
-}); 
+});
+
+module.exports = app;
